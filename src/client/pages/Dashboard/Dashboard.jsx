71e359016166e5716343cd8bdf8893dabcc32710
Alan Eicker <alaneicker@gmail.com>
2020-12-15 11:06:49 -0600

    Refactoring

--- src/client/pages/Dashboard/Dashboard.jsx
+++ src/client/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
-import ExpenseGroups from '../../components/ExpenseGroups';
+import React, { useContext } from 'react';
+import { List, ListItem } from '@atomikui/core';
+import { AppContext } from '../../AppProvider';
+import ExpenseGroupDetail from '../../components/ExpenseGroupDetail';
 import withRouteGuard from '../../withRouteGaurd';
 
-const Dashboard = withRouteGuard(() => <ExpenseGroups />);
+const Dashboard = withRouteGuard(() => {
+  const { data } = useContext(AppContext);
+
+  const getTotalBalance = (expenses) => {
+    return expenses.reduce((total, expense) => {
+      return total + expense.balance;
+    }, 0);
+  };
+
+  const getUpaidBalance = (expenses) => {
+    return expenses
+      .filter((exp) => {
+        return !exp.paid;
+      })
+      .reduce((total, expense) => {
+        return total + expense.balance;
+      }, 0);
+  };
+
+  return React.useMemo(() => {
+    return (
+      <div className="padding-bottom-24">
+        <List loose>
+          {data.map(({ id, title, totalBudget, expenses }, index) => (
+            <ListItem key={`expense-group-${index + 1}`}>
+              <ExpenseGroupDetail
+                id={id}
+                index={index}
+                groupTitle={title}
+                totalBudget={totalBudget}
+                totalBalance={getTotalBalance(expenses)}
+                unpaidBalance={getUpaidBalance(expenses)}
+                expenses={expenses}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+  }, [data]);
+});
 
 export default Dashboard;
